d0eaba3aab4fcd537bbb7d08e07c94bc3485c8c8
Jose <barsamic03@gmail.com>
2025-12-20 17:14:06 +0800

    feat: use bufio reader function for user input

--- app/main.go
+++ app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -11,8 +13,15 @@ func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	for {
 		fmt.Print("$ ")
-		var input string
-		fmt.Scanln(&input)
-		fmt.Printf("%s: command not found \n", input)
+		//var input string
+		//fmt.Scanln(&input)
+		//fmt.Printf("%s: command not found \n", input)
+
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 }
