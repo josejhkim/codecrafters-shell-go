712900784588913a34a29c669e99a606253ad82b
Jaeho <jaeho.kim@beepbeep.tech>
2025-12-29 09:43:25 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/chzyer/readline"
@@ -34,23 +36,55 @@ func main() {
 		stderrDest := os.Stderr
 
 		if strings.Contains(command, "|") {
-			r, w, err := os.Pipe()
-			if err != nil {
-				fmt.Fprintln(stdoutDest, err)
-				os.Exit(1)
+
+			// split the pipeline into individual commands
+			// e.g. "ls -la | grep foo | wc -l" -> ["ls -la ", " grep foo ", " wc -l"]
+			commands := strings.Split(command, "|")
+
+			var cmds []*exec.Cmd
+
+			var prevPipeReader io.Reader = nil
+
+			for i, cmdStr := range commands {
+				cmdStr = strings.TrimSpace(cmdStr)
+
+				var stdoutDest io.Writer = os.Stdout
+				var stderrDest io.Writer = os.Stderr
+
+				var nextPipeReader *os.File
+				var nextPipeWriter *os.File
+				var err error
+
+				if i < len(commands)-1 {
+					nextPipeReader, nextPipeWriter, err = os.Pipe()
+					if err != nil {
+						fmt.Fprintln(os.Stderr, "Error creating pipe:", err)
+						break
+					}
+					stdoutDest = nextPipeWriter
+				}
+
+				cmd := execute.ExecuteUserInput(cmdStr, false, prevPipeReader, stdoutDest, stderrDest)
+
+				if cmd != nil {
+					cmds = append(cmds, cmd)
+				}
+
+				if prevPipeReader != nil {
+					if c, ok := prevPipeReader.(io.Closer); ok {
+						c.Close()
+					}
+				}
+
+				if nextPipeWriter != nil {
+					nextPipeWriter.Close()
+				}
+
+				prevPipeReader = nextPipeReader
 			}
-			before, after, _ := strings.Cut(command, "|")
-			firstCmd := strings.TrimSpace(before)
-			secondCmd := strings.TrimSpace(after)
 
-			cmd1 := execute.ExecuteUserInput(firstCmd, false, nil, w, w)
-			w.Close()
-
-			execute.ExecuteUserInput(secondCmd, true, r, stdoutDest, stderrDest)
-			r.Close()
-
-			if cmd1 != nil {
-				cmd1.Wait()
+			for _, cmd := range cmds {
+				cmd.Wait()
 			}
 		} else {
 			execute.ExecuteUserInput(command, true, nil, stdoutDest, stderrDest)
